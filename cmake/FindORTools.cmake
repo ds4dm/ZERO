33423752771a698b4e72647c183e897259564770
Gabriele Dragotto <gabriele@dragotto.net>
2021-04-18 16:01:04 -0400

    Moved to Conda from Conan

--- cmake/FindORTools.cmake
+++
@@ -1,8 +0,0 @@
-message("Loading Google OR Tools")
-include(FetchContent)
-FetchContent_Declare(
-  or-tools
-  GIT_REPOSITORY https://github.com/google/or-tools.git
-  GIT_TAG master)
-
-FetchContent_MakeAvailable(or-tools)
