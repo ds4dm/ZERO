9219ee49cb124e7f70e2f409b67cbbec0086b370
Gabriele Dragotto <gabriele@dragotto.net>
2021-04-26 17:54:25 -0400

    Updated RandomKP Tests

--- test/IPG/RandomKP.cpp
+++ test/IPG/RandomKP.cpp
@@ -12,113 +12,126 @@
 
 
 #include <armadillo>
-#include <interfaces/epec_models.h>
+#include <games/ipg.h>
 #include <interfaces/ipg_models.h>
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#include <zero.h>
 
 
 int main(int argc, char **argv) {
 
-
-  std::vector<unsigned int>          ItemSizes = {20, 40, 80, 100};
-  std::vector<unsigned int>          Players   = {2, 3, 4};
+  loguru::g_stderr_verbosity                   = -5;
+  std::vector<unsigned int>          ItemSizes = {10, 20, 40, 80, 100};
+  std::vector<unsigned int>          Players   = {2, 3};
   unsigned int                       insUB     = 10;
   bool                               print     = false;
-  bool                               generate  = false;
   GRBEnv                             test;
-  std::string                        BasePath = "dat/KP/";
-  std::string                        Logfile  = "dat/KP/results.csv";
+  std::string                        BasePath      = "dat/KPMarg/dest/";
+  std::string                        MargInstances = "dat/KPMarg/source/";
+  std::string                        Logfile       = "dat/KPMarg/resultsMIPCuts.csv";
   std::random_device                 random;
   std::mt19937                       Oracle(random());
   std::uniform_int_distribution<int> hRandom(-100, 100);
   std::uniform_int_distribution<int> fRandom(-50, 50);
 
-  for (unsigned int n : ItemSizes) {
-	 for (unsigned int ins = 0; ins < insUB; ++ins) {
-		for (auto &&m : Players) {
+  for (auto &&m : Players) {
+	 for (unsigned int n : ItemSizes) {
+		for (unsigned int ins = 0; ins < insUB; ++ins) {
 
 		  std::string InstanceName =
 				"Instance_" + std::to_string(m) + "-" + std::to_string(n) + "-" + std::to_string(ins);
-		  if (generate) {
-			 Models::IPG::IPGInstance Instance;
-			 for (unsigned int j = 0; j < m; ++j) {
-				arma::vec _c(n);
-				arma::mat _Q(n, n * (m - 1));
-				arma::mat _A(1, n);
-
-				// Bounds on integer variables to one
-				arma::mat _Abounds(n, n, arma::fill::eye);
-				arma::vec _ones(n, arma::fill::ones);
-				arma::vec _integers(n);
-
-				for (unsigned int i = 0; i < n; ++i)
-				  _integers.at(i) = i;
-
-				for (auto &element : _Q)
-				  element = hRandom(Oracle);
-
-				for (auto &element : _A)
-				  element = hRandom(Oracle);
-
-				for (auto &element : _c) {
-				  element = fRandom(Oracle);
-				}
-
-				double b = arma::as_scalar(arma::accu(_A)) * ins / 11;
-
-				// Explicitly join the upper bounds
-				_A           = arma::join_cols(_A, _Abounds);
-				arma::vec _b = arma::join_cols(arma::vec{b}, _ones);
-
-				if (print) {
-				  _c.print("c:");
-				  _b.print("b:");
-				  _Q.print("Q");
-				  _A.print("A");
-				}
-
-				arma::sp_mat      _Q2 = arma::sp_mat{_Q};
-				arma::sp_mat      _A2 = arma::sp_mat{_A};
-				MathOpt::IP_Param ipParam(_Q2, _A2, _b, _c, _integers, &test);
-				Instance.addIPParam(ipParam,
-										  BasePath + "raw/" + InstanceName + "-p" + std::to_string(j));
-			 }
-			 Instance.save(BasePath + InstanceName);
-			 std::cout << "\tSaving instance " << InstanceName << "\n";
-			 // Save the instance
-		  } else {
+		  {
 			 try {
-				Models::IPG::IPG Test(&test, BasePath + InstanceName);
-				Test.setAlgorithm(Data::IPG::Algorithms::Oracle);
-				Test.setDeviationTolerance(3e-4);
-				Test.setIndicators(true);
-				Test.setNumThreads(8);
-				Test.setTimeLimit(60);
-				Test.finalize();
-				Test.findNashEq();
 
-				auto          stat = Test.getStatistics();
-				std::ifstream existCheck(Logfile);
-				std::ofstream results(Logfile, std::ios::app);
+				// InstanceName      = "Instance_2-80-9";
+				std::string filec = BasePath + InstanceName + ".json";
+				std::cout << filec << std::endl;
+				if (access(filec.c_str(), F_OK) == 0) {
 
-				if (!existCheck.good()) {
-				  results << "instance;Algorithm;Players;isPureNE;RequiredPureNE;"
-								 "Status;"
-								 "NumVar;ClockTime(s);Threads;Indicators;numInnerIterations\n";
+				  std::vector<Data::LCP::Algorithms> algos = {Data::LCP::Algorithms::MIP,
+																			 // Data::LCP::Algorithms::MINLP,
+																			 Data::LCP::Algorithms::PATH};
+
+				  std::vector<Data::IPG::Objectives> objectives = {Data::IPG::Objectives::Quadratic,
+																					// Data::IPG::Objectives::Linear,
+																					Data::IPG::Objectives::Feasibility};
+
+				  std::vector<Data::IPG::CutsAggressiveness> cuts = {
+						Data::IPG::CutsAggressiveness::KeepItCool,
+						Data::IPG::CutsAggressiveness::Truculent};
+
+				  Models::IPG::IPGInstance Instance;
+				  Instance.load(BasePath + InstanceName);
+				  for (auto algo : algos) {
+					 for (auto obj : objectives) {
+						for (auto cut : cuts) {
+						  if (!((obj == Data::IPG::Objectives::Quadratic ||
+									obj == Data::IPG::Objectives::Linear) &&
+								  algo == Data::LCP::Algorithms::PATH)) {
+							 std::cout << "\t Testing: Algorithm " << std::to_string(algo)
+										  << " with objective " << std::to_string(obj) << ", CutsLevel"
+										  << std::to_string(cut) << "\n";
+							 Models::IPG::IPG Test(&test, Instance);
+							 Test.setAlgorithm(Data::IPG::Algorithms::Oracle);
+							 Test.setDeviationTolerance(3e-4);
+							 Test.setNumThreads(8);
+							 Test.setCutsAggressiveness(cut);
+							 Test.setTimeLimit(300);
+							 Test.setLCPAlgorithm(algo);
+							 Test.setGameObjective(obj);
+							 Test.finalize();
+							 std::chrono::steady_clock::time_point begin =
+								  std::chrono::steady_clock::now();
+							 Test.findNashEq();
+							 std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+							 // std::cout <<
+							 // std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count()/1000000.0
+							 // <<"s";
+
+							 auto stat = Test.getStatistics();
+
+							 // std::cout << "\n" <<stat.WallClockTime.get();
+							 std::ifstream existCheck(Logfile);
+							 std::ofstream results(Logfile, std::ios::app);
+
+							 auto cuts      = stat.AlgorithmData.Cuts.get();
+							 int  totalCuts = 0;
+							 for (auto cut : cuts)
+								totalCuts += cut.second;
+
+							 if (!existCheck.good()) {
+								results << "Instance;Algorithm;LCPAlgo;ObjType;Cuts;Players;isPureNE;"
+											  "Status;"
+											  "NumVar;ClockTime(s);Threads;numIterations;ValueCuts;VPolyCuts;"
+											  "MIRCuts;GMICuts;KPCuts;TotalCuts;\n";
+							 }
+							 existCheck.close();
+
+
+							 results << InstanceName << ";"
+										<< std::to_string(stat.AlgorithmData.Algorithm.get()) << ";"
+										<< std::to_string(algo) << ";" << std::to_string(obj) << ";"
+										<< std::to_string(cut) << ";" << Test.getNumPlayers() << ";"
+										<< std::to_string(stat.PureNashEquilibrium.get()) << ";"
+										<< std::to_string(stat.Status.get()) << ";" << stat.NumVar.get()
+										<< ";" << std::to_string(stat.WallClockTime.get()) << ";"
+										<< std::to_string(stat.AlgorithmData.Threads.get()) << ";"
+										<< stat.NumIterations.get() << ";" << cuts.at(0).second << ";"
+										<< cuts.at(1).second << ";" << cuts.at(2).second << ";"
+										<< cuts.at(3).second << ";" << cuts.at(4).second << ";" << totalCuts
+										<< "\n";
+
+
+
+							 results.close();
+						  } // endif objectives and PATH
+						}   // algo
+					 }     // cuts
+				  }
 				}
-				existCheck.close();
-
-
-				results << InstanceName << ";" << std::to_string(stat.AlgorithmData.Algorithm.get())
-						  << ";" << Test.getNumPlayers() << ";"
-						  << std::to_string(stat.PureNashEquilibrium.get()) << ";"
-						  << "0"
-						  << ";" << std::to_string(stat.Status.get()) << ";" << stat.NumVar.get()
-						  << ";inf;" << std::to_string(stat.AlgorithmData.Threads.get()) << ";"
-						  << stat.NumIterations.get() << "\n";
-
-				results.close();
 			 } catch (ZEROException e) {
 				std::cout << "ZEROException: " << std::to_string(e.which()) << " | " << e.more();
 			 }
