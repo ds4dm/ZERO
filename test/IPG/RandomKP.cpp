6d15a05c03537a100304f55dcf7735908dd82527
Gabriele Dragotto <gabriele@dragotto.net>
2021-05-09 22:00:58 -0400

    RandomKP Tests

--- test/IPG/RandomKP.cpp
+++ test/IPG/RandomKP.cpp
@@ -22,7 +22,7 @@
 
 int main(int argc, char **argv) {
 
-  loguru::g_stderr_verbosity                   = -5;
+  loguru::g_stderr_verbosity                   = 0;
   std::vector<unsigned int>          ItemSizes = {10, 20, 40, 80, 100};
   std::vector<unsigned int>          Players   = {2, 3};
   unsigned int                       insUB     = 10;
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
   GRBEnv                             test;
   std::string                        BasePath      = "dat/KPMarg/dest/";
   std::string                        MargInstances = "dat/KPMarg/source/";
-  std::string                        Logfile       = "dat/KPMarg/resultsMIPCuts.csv";
+  std::string                        Logfile       = "dat/KPMarg/results.csv";
   std::random_device                 random;
   std::mt19937                       Oracle(random());
   std::uniform_int_distribution<int> hRandom(-100, 100);
@@ -45,91 +45,99 @@ int main(int argc, char **argv) {
 		  {
 			 try {
 
-				// InstanceName      = "Instance_2-80-9";
+
 				std::string filec = BasePath + InstanceName + ".json";
-				std::cout << filec << std::endl;
+				std::cout << "-------------------" << filec << "------------------\n\n";
 				if (access(filec.c_str(), F_OK) == 0) {
 
-				  std::vector<Data::LCP::Algorithms> algos = {Data::LCP::Algorithms::MIP,
-																			 // Data::LCP::Algorithms::MINLP,
-																			 Data::LCP::Algorithms::PATH};
+				  std::vector<Data::LCP::Algorithms> algos = {
+						//Data::LCP::Algorithms::MIP,
+						//Data::LCP::Algorithms::MINLP,
+						Data::LCP::Algorithms::PATH
+				  };
 
-				  std::vector<Data::IPG::Objectives> objectives = {Data::IPG::Objectives::Quadratic,
-																					// Data::IPG::Objectives::Linear,
-																					Data::IPG::Objectives::Feasibility};
+				  std::vector<Data::IPG::Objectives> objectives = {
+						//Data::IPG::Objectives::Quadratic,
+						//Data::IPG::Objectives::Linear,
+						Data::IPG::Objectives::Feasibility
+				  };
 
 				  std::vector<Data::IPG::CutsAggressiveness> cuts = {
+						Data::IPG::CutsAggressiveness::NoThanks,
 						Data::IPG::CutsAggressiveness::KeepItCool,
-						Data::IPG::CutsAggressiveness::Truculent};
+						Data::IPG::CutsAggressiveness::Truculent
+				  };
+
+
+				  //InstanceName      = "Instance_2-100-8";
 
 				  Models::IPG::IPGInstance Instance;
 				  Instance.load(BasePath + InstanceName);
 				  for (auto algo : algos) {
 					 for (auto obj : objectives) {
 						for (auto cut : cuts) {
-						  if (!((obj == Data::IPG::Objectives::Quadratic ||
-									obj == Data::IPG::Objectives::Linear) &&
-								  algo == Data::LCP::Algorithms::PATH)) {
-							 std::cout << "\t Testing: Algorithm " << std::to_string(algo)
-										  << " with objective " << std::to_string(obj) << ", CutsLevel"
-										  << std::to_string(cut) << "\n";
-							 Models::IPG::IPG Test(&test, Instance);
-							 Test.setAlgorithm(Data::IPG::Algorithms::Oracle);
-							 Test.setDeviationTolerance(3e-4);
-							 Test.setNumThreads(8);
-							 Test.setCutsAggressiveness(cut);
-							 Test.setTimeLimit(300);
-							 Test.setLCPAlgorithm(algo);
-							 Test.setGameObjective(obj);
-							 Test.finalize();
-							 std::chrono::steady_clock::time_point begin =
-								  std::chrono::steady_clock::now();
-							 Test.findNashEq();
-							 std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+						  std::cout << "\tTesting:" << std::to_string(algo) <<"-"<<std::to_string(obj)<<"-"<<std::to_string(cut)<<"\n";
+						  auto realobj = obj;
+						  if (algo == Data::LCP::Algorithms::PATH &&
+						      obj == Data::IPG::Objectives::Quadratic)
+							 realobj = Data::IPG::Objectives::Feasibility;
+						  Models::IPG::IPG Test(&test, Instance);
+						  Test.setAlgorithm(Data::IPG::Algorithms::Oracle);
+						  Test.setDeviationTolerance(3e-4);
+						  Test.setNumThreads(2);
+						  Test.setCutsAggressiveness(cut);
+						  Test.setTimeLimit(1000);
+						  Test.setLCPAlgorithm(algo);
+						  Test.setGameObjective(realobj);
+						  Test.finalize();
+						  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+						  Test.findNashEq();
+						  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-							 // std::cout <<
-							 // std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count()/1000000.0
-							 // <<"s";
+						  // std::cout <<
+						  // std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count()/1000000.0
+						  // <<"s";
 
-							 auto stat = Test.getStatistics();
+						  auto stat = Test.getStatistics();
+						  double time = stat.WallClockTime.get();
+						  std::cout << "\t\t Time:" <<std::to_string(time) << " Status:" << std::to_string(stat.Status.get())<<std::endl;
 
-							 // std::cout << "\n" <<stat.WallClockTime.get();
-							 std::ifstream existCheck(Logfile);
-							 std::ofstream results(Logfile, std::ios::app);
+						  std::ifstream existCheck(Logfile);
+						  std::ofstream results(Logfile, std::ios::app);
 
-							 auto cuts      = stat.AlgorithmData.Cuts.get();
-							 int  totalCuts = 0;
-							 for (auto cut : cuts)
-								totalCuts += cut.second;
+						  auto cuts      = stat.AlgorithmData.Cuts.get();
+						  int  totalCuts = 0;
+						  for (auto cut : cuts)
+							 totalCuts += cut.second;
 
-							 if (!existCheck.good()) {
-								results << "Instance;Algorithm;LCPAlgo;ObjType;Cuts;Players;isPureNE;"
-											  "Status;"
-											  "NumVar;ClockTime(s);Threads;numIterations;ValueCuts;VPolyCuts;"
-											  "MIRCuts;GMICuts;KPCuts;TotalCuts;\n";
-							 }
-							 existCheck.close();
-
-
-							 results << InstanceName << ";"
-										<< std::to_string(stat.AlgorithmData.Algorithm.get()) << ";"
-										<< std::to_string(algo) << ";" << std::to_string(obj) << ";"
-										<< std::to_string(cut) << ";" << Test.getNumPlayers() << ";"
-										<< std::to_string(stat.PureNashEquilibrium.get()) << ";"
-										<< std::to_string(stat.Status.get()) << ";" << stat.NumVar.get()
-										<< ";" << std::to_string(stat.WallClockTime.get()) << ";"
-										<< std::to_string(stat.AlgorithmData.Threads.get()) << ";"
-										<< stat.NumIterations.get() << ";" << cuts.at(0).second << ";"
-										<< cuts.at(1).second << ";" << cuts.at(2).second << ";"
-										<< cuts.at(3).second << ";" << cuts.at(4).second << ";" << totalCuts
-										<< "\n";
+						  if (!existCheck.good()) {
+							 results << "Instance;m;n;ins;Algorithm;LCPAlgo;ObjType;Cuts;Players;isPureNE;"
+							            "Status;"
+							            "NumVar;Time;Threads;numIterations;ValueCuts;VPolyCuts;"
+							            "MIRCuts;GMICuts;KPCuts;TotalCuts;\n";
+						  }
+						  existCheck.close();
 
 
+						  results << InstanceName << ";" << std::to_string(m) << ";" << std::to_string(n)
+						          << ";" << std::to_string(ins) << ";"
+						          << std::to_string(stat.AlgorithmData.Algorithm.get()) << ";"
+						          << std::to_string(algo) << ";" << std::to_string(obj) << ";"
+						          << std::to_string(cut) << ";" << Test.getNumPlayers() << ";"
+						          << std::to_string(stat.PureNashEquilibrium.get()) << ";"
+						          << std::to_string(stat.Status.get()) << ";" << stat.NumVar.get() << ";"
+						          << std::to_string(stat.WallClockTime.get()) << ";"
+						          << std::to_string(stat.AlgorithmData.Threads.get()) << ";"
+						          << stat.NumIterations.get() << ";" << cuts.at(0).second << ";"
+						          << cuts.at(1).second << ";" << cuts.at(2).second << ";"
+						          << cuts.at(3).second << ";" << cuts.at(4).second << ";" << totalCuts
+						          << "\n";
 
-							 results.close();
-						  } // endif objectives and PATH
-						}   // algo
-					 }     // cuts
+
+
+						  results.close();
+						} // algo
+					 }   // cuts
 				  }
 				}
 			 } catch (ZEROException e) {
